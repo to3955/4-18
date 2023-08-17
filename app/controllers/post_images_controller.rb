e0fd0f6bbd18107716bdb07af61ab63d6b25c301
EC2 Default User <ec2-user@ip-172-31-32-162.ap-northeast-1.compute.internal>
2023-08-17 00:51:46 +0000

    Start4-18

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,36 @@
+class PostImagesController < ApplicationController
+  
+  def new
+    @post_image = PostImage.new
+  end
+
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.new
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_images = PostImage.find(params[:id])
+  end
+  
+  def destroy
+    post_image = 削除するPostImageレコードを取得
+    post_image.削除
+    redirect_to PostImageの一覧ページへのパス
+  end
+  
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
+end
